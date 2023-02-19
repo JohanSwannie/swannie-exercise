b773f1c6fd4a0238523ce96d7121ac51187ed7c2
jswan <jswannieman@gmail.com>
2023-02-19 15:22:59 +1100

    Update App JS Component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 // @flow
-
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
-import CustomerSignin from "./routes/signin/customer-signin.component";
-import AdminSignin from "./routes/signin/admin-signin.component";
+import Signin from "./routes/signin/signin.component";
+import Customer from "./routes/customer/customer.component";
+import Administrator from "./routes/administrator/administrator.component";
 import Checkout from "./routes/checkout/checkout.component";
-export const Array = [99, true, "App", 45.35, false];
+// export const Array = [99, true, "App", 45.35, false];
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/auth1" element={<CustomerSignin />} />
-        <Route path="/auth2" element={<AdminSignin />} />
+        <Route path="/auth" element={<Signin />} />
         <Route path="/checkout" element={<Checkout />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/admin" element={<Administrator />} />
       </Routes>
     </div>
   );
